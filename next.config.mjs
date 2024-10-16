f03e9915e420208875989d8f01f47202e6e6010f
katerinaRaven <ekaterina.yu.voronchikhina@yandex.ru>
2024-10-16 20:15:02 +0300

    v. 0.1.2 : config settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: { domains: [
+            '89.111.170.250',
+            'eraven.ru',
+        ]},
+    reactStrictMode: false,
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    cssModules: true,
+};
 
 export default nextConfig;
